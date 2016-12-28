3c5d6a5f1a21c16117a891652797619928877331
Schwifty <schwifty@schwifty.space>
2016-12-28 12:52:46 +0100

    Replace secondsToHHMMS with pretty-ms

--- bot.js
+++ bot.js
@@ -1499,22 +1499,6 @@ function saveData(file, callback) {
 	});
 }
 
-function secondsToHHMMSS(totalSeconds) {
-	var hours   = Math.floor(totalSeconds / 3600);
-	var minutes = Math.floor((totalSeconds - (hours * 3600)) / 60);
-	var seconds = totalSeconds - (hours * 3600) - (minutes * 60);
-
-	// round seconds
-	seconds = Math.round(seconds * 100) / 100
-
-	var result = (hours < 10 ? "0" + hours : hours);
-
-	result += "h " + (minutes < 10 ? "0" + minutes : minutes);
-	result += "m " + (seconds  < 10 ? "0" + seconds : seconds) + "s";
-
-	return result;
-}
-
 // Adjust the volume
 function setVolume(msg, volume) {
 	if (!voice[msg.guild.id]) {
